a7415c63f5904614355ac7b79b3939825fa464ec
Adnakateg <getakanda@outlook.com>
2024-05-25 19:02:12 +0530

    Fix file structure

--- benkyou/basics/src/Arrays.cpp
+++ benkyou/basics/src/Arrays.cpp
@@ -1,24 +1,24 @@
 #include "Arrays.h"
 
-int main(int argc, char** argv)
+using std::cout;
+using std::endl;
+
+void arrays()
 {
-  int A[5] = {};
+    int A[5] = {};
 
-  A[0] = 1;
-  A[1] = 2;
-  A[2] = 3;
-  A[3] = 4;
-  A[4] = 5;
+    A[0] = 1;
+    A[1] = 2;
+    A[2] = 3;
+    A[3] = 4;
+    A[4] = 5;
 
-  std::cout << "Size of int : " << sizeof(int) << std::endl;
-  std::cout << "Size of int[5] : " << sizeof(A) << std::endl;
+    cout << "Size of int : " << sizeof(int) << endl;
+    cout << "Size of int[5] : " << sizeof(A) << endl;
 
-
-  for (int i = 0; i < 5; i++)
-  {
-    std::cout << "Size of A[" << i << "] : " << sizeof(A[1]) << std::endl;
-    std::cout << "A[" << i << "] : " << A[i] << std::endl;
-  }
-
-  return 0;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Size of A[" << i << "] : " << sizeof(A[1]) << endl;
+        cout << "A[" << i << "] : " << A[i] << endl;
+    }
 }
---
+++ benkyou/basics/src/Main.cpp
@@ -0,0 +1,8 @@
+#include "Arrays.h"
+
+int main(int argc, char** argv)
+{
+    arrays();
+
+    return 0;
+}
