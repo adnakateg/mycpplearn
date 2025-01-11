65a0a97ca80be336bee33e1ab487aa9877de944c
Adnakateg <getakanda@outlook.com>
2025-01-11 17:55:47 +0530

    Update

--- include/dsa/dsa.hpp
+++ include/dsa/dsa.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "hashing/find_common_numbers.hpp"
+#include "hashing/group_of_anagrams.hpp"
 #include "hashing/hash_test.hpp"
-
+#include "hashing/std_unordered_map_test.hpp"
+#include "hashing/two_sum.hpp"
 #include "linked_lists/doubly_linked_list_test.hpp"
 #include "linked_lists/singly_linked_list_test.hpp"
 
--- include/dsa/hashing/group_of_anagrams.hpp
+++ include/dsa/hashing/group_of_anagrams.hpp
@@ -2,3 +2,49 @@
  * Implement a function called groupAnagrams() that groups a list of strings
  * based on their anagram equivalence.
  */
+#pragma once
+#include <gtest/gtest.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using std::cout;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+vector<vector<string>>
+getAnagrams([[__maybe_unused__]] const vector<string> &strings) {
+
+  auto hasher = [](const string &str) -> auto {
+    string _str = str;
+    std::sort(_str.begin(), _str.end());
+    return std::hash<std::string>{}(_str);
+  };
+
+  unordered_map<size_t, size_t> uoMap{};
+  vector<vector<string>> res{};
+
+  for (auto &e : strings) {
+    auto key = hasher(e);
+    if (uoMap.find(key) == uoMap.end()) {
+      uoMap[key] = res.size();
+      res.push_back({});
+    }
+    res[uoMap[key]].push_back(e);
+  }
+  return res;
+}
+
+TEST(group_of_anagrams_test, test01) {
+  vector<string> input{"eat", "tea", "tan", "ate", "nat", "bat"};
+  vector<vector<string>> anagrams = getAnagrams(input);
+
+  for (const auto &e1 : anagrams) {
+    for (const auto &e2 : e1) {
+      cout << e2 << " ";
+    }
+    cout << "\n";
+  }
+}
