277659678e67bb8cff1bd7a17723b3743cf76de1
adnakateg <getakanda@outlook.com>
2025-02-19 05:41:31 +0530

    Update

---
+++ include/all.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "dsa.hpp"
+#include "ecpp.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
-#include "dsa.hpp"
+#include "all.hpp"
 #include <gtest/gtest.h>
 #include <iostream>
 using std::cout;
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
 
-  auto ret = RUN_ALL_TESTS();
-  cout << "\n\n====================\n";
-  if (ret == 0) {
-    cout << "TESTING SUCCESSFULL\n";
-  } else {
-    cout << "TESTING FAILED\n";
-  }
-  cout << "====================\n\n";
-  return ret;
+    auto ret = RUN_ALL_TESTS();
+    cout << "\n\n====================\n";
+    if (ret == 0)
+    {
+        cout << "TESTING SUCCESSFULL\n";
+    }
+    else
+    {
+        cout << "TESTING FAILED\n";
+    }
+    cout << "====================\n\n";
+    return ret;
 }
